eb1b874eaf5aada3f5be2a433682fda694cf445e
enacreditavel <enacreditavel@gmail.com>
2025-07-08 09:12:54 -0300

    Adicionando novas manchetes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "id": "1",
+    "title": "REVIEW - IRON HEART",
+    "description": "O que esperar da nova série do Disney+",
+    "photoCover": "https://i0.wp.com/explosionnetwork.com/wp-content/uploads/2025/05/ironheart-trailer-thumbnail.jpg?fit=1080%2C608&ssl=1"
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "2",
+    "title": "Thunderbolts: quando estreia o novo filme da Marvel no streaming?",
+    "description": "Sem data de lançamento definida no streaming, Thunderbolts é um dos filmes mais esperados do MCU.",
+    "photoCover": "https://www.em.com.br/emfoco/wp-content/uploads/2025/04/Thunderbolts-1.jpg"
   }
 ]
